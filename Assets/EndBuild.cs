3b05d30e61632f701c56e5f2b197778895e9e826
aaliyahsykes04 <ats136@pitt.edu>
2025-12-04 20:48:25 -0500

    2nd to last day
    -Skybox done
    -trees have colider
    -sky has text

--- Assets/EndBuild.cs
+++ Assets/EndBuild.cs
@@ -7,7 +7,7 @@ public class EndBuild : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,4 +17,5 @@ public class EndBuild : MonoBehaviour
         {
             Application.Quit();
         }
-     c}
+    }
+}
